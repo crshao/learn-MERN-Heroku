ac637fb6ed3ce552cc2d9c6dfb67a3ae85fac76a
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2021-01-07 21:55:35 +0700

    small fix finishing heroku deployment

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://hao:testtest00@cluster0/learn-mern-heroku?retryWrites=true&w=majority";
+const connection = "mongodb+srv://hao:yq4DrhiitxpdkwwN@cluster0/learn-mern-heroku?retryWrites=true&w=majority";
 mongoose.connect(connection, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
